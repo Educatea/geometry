12a0fa459097b14218c0a8e01b06615082491501
Juan Gesino <juangesino@gmail.com>
2015-05-11 01:41:18 -0300

    Source

--- lib/geometry.rb
+++ lib/geometry.rb
@@ -1,5 +1,9 @@
 require "geometry/version"
 require "geometry/geometry_circle"
+require "geometry/geometry_ellipse"
+require "geometry/geometry_rectangle"
+require "geometry/geometry_right_triangle"
+require "geometry/geometry_square"
 require 'ostruct'
 require 'active_support/core_ext/hash'
 
