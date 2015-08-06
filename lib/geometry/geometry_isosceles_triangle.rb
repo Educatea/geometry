8f5ea1b12ca2c5d731cf59a1005998e3d9754639
Juan Gesino <juangesino@gmail.com>
2015-08-06 20:25:33 -0300

    v0.1.2

--- lib/geometry/geometry_isosceles_triangle.rb
+++ lib/geometry/geometry_isosceles_triangle.rb
@@ -34,7 +34,7 @@ module Geometry::IsoscelesTriangle
 			new_label = "#{new_label}<div id='itside2' style='z-index: 99;position: relative;top: 0px;left: 85px;color: #{@label_color};font-weight: bold;'>#{side2.to_s.empty? ? '&nbsp;' : side2}</div>"
 		else
 			new_label = "<div id='itbase' style='z-index: 99;position: relative;left: 45px;top: 110px;color: #{@label_color};font-weight: bold;'>#{base.to_s.empty? ? '&nbsp;' : base}</div>"
-			new_label = "#{new_label}<div id='itside1' style='z-index: 99;position: relative;top: 15px;left: 0px;color: #{@label_color};font-weight: bold;'>#{side1.to_s.empty? ? '&nbsp;' : side1}</div>"	
+			new_label = "#{new_label}<div id='itside1' style='z-index: 99;position: relative;top: 20px;left: 0px;color: #{@label_color};font-weight: bold;'>#{side1.to_s.empty? ? '&nbsp;' : side1}</div>"	
 			new_label = "#{new_label}<div id='itside2' style='z-index: 99;position: relative;top: 0px;left: 85px;color: #{@label_color};font-weight: bold;'>#{side2.to_s.empty? ? '&nbsp;' : side2}</div>"
 		end
 		@html = "#{@html.reverse.sub('>vid/<', '').reverse}#{new_label}</div>"	
