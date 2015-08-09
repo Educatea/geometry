7f338f8831906f1a79894500c0114f4a3fbc8d57
Juan Gesino <juangesino@gmail.com>
2015-08-09 10:00:37 -0300

    Trapezoid

--- lib/geometry/geometry_trapezoid.rb
+++ lib/geometry/geometry_trapezoid.rb
@@ -11,7 +11,7 @@ module Geometry::Trapezoid
 		@html = "<div id='mygraphiccontainer#{@id}' class='#{graphic_class}' style='position: relative;width: 100px;height:100px;#{style}'></div>"
 		case @type
 		when 2
-			@js = "<script>YUI().use('graphics',function(e){var o=new e.Graphic({autoSize:!0,render:'#mygraphiccontainer#{@id}'}),i=o.addShape({type:'path',stroke:{weight:2,color:'#{line_color}',opacity:1}});i.moveTo(50,0),i.lineTo(0,100),i.lineTo(200,100),i.lineTo(150,0),i.lineTo(50,0),i.end()});</script>"
+			@js = "<script>YUI().use('graphics',function(e){var o=new e.Graphic({autoSize:!0,render:'#mygraphiccontainer#{@id}'}),i=o.addShape({type:'path',stroke:{weight:2,color:'#{line_color}',opacity:1}});i.moveTo(50,100),i.lineTo(150,100),i.lineTo(200,0),i.lineTo(0,0),i.lineTo(50,100),i.end()});</script>"
 		else
 			@js = "<script>YUI().use('graphics',function(e){var o=new e.Graphic({autoSize:!0,render:'#mygraphiccontainer#{@id}'}),i=o.addShape({type:'path',stroke:{weight:2,color:'#{line_color}',opacity:1}});i.moveTo(50,0),i.lineTo(0,100),i.lineTo(200,100),i.lineTo(150,0),i.lineTo(50,0),i.end()});</script>"
 		end
@@ -30,31 +30,35 @@ module Geometry::Trapezoid
 		"#{@html}#{@js}"
 	end
 
-	def self.add_labels(base, side1, side2)
+	def self.add_labels(base1, side1, side2, base2)
 		case @type
 		when 2
-			new_label = "<div id='itbase' style='z-index: 99;position: relative;left: 40px;color: #{@label_color};font-weight: bold;'>#{base.to_s.empty? ? '&nbsp;' : base}</div>"
-			new_label = "#{new_label}<div id='itside1' style='z-index: 99;position: relative;top: 15px;left: 0px;color: #{@label_color};font-weight: bold;'>#{side1.to_s.empty? ? '&nbsp;' : side1}</div>"	
-			new_label = "#{new_label}<div id='itside2' style='z-index: 99;position: relative;top: 0px;left: 85px;color: #{@label_color};font-weight: bold;'>#{side2.to_s.empty? ? '&nbsp;' : side2}</div>"
+			new_label = "<div id='base1' style='z-index: 99;position: relative;left: 95px;top: -25px;color: #{@label_color};font-weight: bold;'>#{base1.to_s.empty? ? '&nbsp;' : base1}</div>"
+			new_label = "#{new_label}<div id='side1' style='z-index: 99;position: relative;top: 20px;left: 190px;color: #{@label_color};font-weight: bold;'>#{side1.to_s.empty? ? '&nbsp;' : side1}</div>"	
+			new_label = "#{new_label}<div id='side2' style='z-index: 99;position: relative;top: 2px;left: 0px;color: #{@label_color};font-weight: bold;'>#{side2.to_s.empty? ? '&nbsp;' : side2}</div>"
+			new_label = "#{new_label}<div id='base2' style='z-index: 99;position: relative;top: 60px;left: 100px;color: #{@label_color};font-weight: bold;'>#{base2.to_s.empty? ? '&nbsp;' : base2}</div>"
 		else
-			new_label = "<div id='itbase' style='z-index: 99;position: relative;left: 45px;top: 110px;color: #{@label_color};font-weight: bold;'>#{base.to_s.empty? ? '&nbsp;' : base}</div>"
-			new_label = "#{new_label}<div id='itside1' style='z-index: 99;position: relative;top: 20px;left: 0px;color: #{@label_color};font-weight: bold;'>#{side1.to_s.empty? ? '&nbsp;' : side1}</div>"	
-			new_label = "#{new_label}<div id='itside2' style='z-index: 99;position: relative;top: 0px;left: 85px;color: #{@label_color};font-weight: bold;'>#{side2.to_s.empty? ? '&nbsp;' : side2}</div>"
+			new_label = "<div id='base1' style='z-index: 99;position: relative;left: 100px;top: 110px;color: #{@label_color};font-weight: bold;'>#{base1.to_s.empty? ? '&nbsp;' : base1}</div>"
+			new_label = "#{new_label}<div id='side1' style='z-index: 99;position: relative;top: 20px;left: 0px;color: #{@label_color};font-weight: bold;'>#{side1.to_s.empty? ? '&nbsp;' : side1}</div>"	
+			new_label = "#{new_label}<div id='side2' style='z-index: 99;position: relative;top: 2px;left: 190px;color: #{@label_color};font-weight: bold;'>#{side2.to_s.empty? ? '&nbsp;' : side2}</div>"
+			new_label = "#{new_label}<div id='base2' style='z-index: 99;position: relative;top: -80px;left: 95px;color: #{@label_color};font-weight: bold;'>#{base2.to_s.empty? ? '&nbsp;' : base2}</div>"
 		end
 		@html = "#{@html.reverse.sub('>vid/<', '').reverse}#{new_label}</div>"	
 		return self
 	end
 
-	def self.add_vertices(v1, v2, v3)
+	def self.add_vertices(v1, v2, v3, v4)
 		case @type
 		when 2
-			new_label = "<div id='v1' style='z-index: 99;position: relative;left: 0px; top: -20px;color: #{@label_color};font-weight: bold;'>#{v1.to_s.empty? ? '&nbsp;' : v1}</div>"
-			new_label = "#{new_label}<div id='v2' style='z-index: 99;position: relative;top: 90px;left: 45px;color: #{@label_color};font-weight: bold;'>#{v2.to_s.empty? ? '&nbsp;' : v2}</div>"	
-			new_label = "#{new_label}<div id='v3' style='z-index: 99;position: relative;top: -55px;left: 95px;color: #{@label_color};font-weight: bold;'>#{v3.to_s.empty? ? '&nbsp;' : v3}</div>"
+			new_label = "<div id='v1' style='z-index: 99;position: relative;left: 210px;top: -25px;color: #{@label_color};font-weight: bold;'>#{v1.to_s.empty? ? '&nbsp;' : v1}</div>"
+			new_label = "#{new_label}<div id='v2' style='z-index: 99;position: relative;top: 95px;left: 150px;color: #{@label_color};font-weight: bold;'>#{v2.to_s.empty? ? '&nbsp;' : v2}</div>"	
+			new_label = "#{new_label}<div id='v3' style='z-index: 99;position: relative;top: 75px;left: 40px;color: #{@label_color};font-weight: bold;'>#{v3.to_s.empty? ? '&nbsp;' : v3}</div>"
+			new_label = "#{new_label}<div id='v4' style='z-index: 99;position: relative;top: -80px;left: -10px;color: #{@label_color};font-weight: bold;'>#{v4.to_s.empty? ? '&nbsp;' : v4}</div>"
 		else
-			new_label = "<div id='v1' style='z-index: 99;position: relative;left: -10px;top: 110px;color: #{@label_color};font-weight: bold;'>#{v1.to_s.empty? ? '&nbsp;' : v1}</div>"
-			new_label = "#{new_label}<div id='v2' style='z-index: 99;position: relative;top: -45px;left: 45px;color: #{@label_color};font-weight: bold;'>#{v2.to_s.empty? ? '&nbsp;' : v2}</div>"	
-			new_label = "#{new_label}<div id='v3' style='z-index: 99;position: relative;top: 75px;left: 100px;color: #{@label_color};font-weight: bold;'>#{v3.to_s.empty? ? '&nbsp;' : v3}</div>"
+			new_label = "<div id='v1' style='z-index: 99;position: relative;left: -15px;top: 110px;color: #{@label_color};font-weight: bold;'>#{v1.to_s.empty? ? '&nbsp;' : v1}</div>"
+			new_label = "#{new_label}<div id='v2' style='z-index: 99;position: relative;top: -40px;left: 35px;color: #{@label_color};font-weight: bold;'>#{v2.to_s.empty? ? '&nbsp;' : v2}</div>"	
+			new_label = "#{new_label}<div id='v3' style='z-index: 99;position: relative;top: -60px;left: 160px;color: #{@label_color};font-weight: bold;'>#{v3.to_s.empty? ? '&nbsp;' : v3}</div>"
+			new_label = "#{new_label}<div id='v4' style='z-index: 99;position: relative;top: 60px;left: 200px;color: #{@label_color};font-weight: bold;'>#{v4.to_s.empty? ? '&nbsp;' : v4}</div>"
 		end
 		@html = "#{@html.reverse.sub('>vid/<', '').reverse}#{new_label}</div>"	
 		return self
